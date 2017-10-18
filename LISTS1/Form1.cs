c00a876dff2c7a1a07d6e76e5576b47391d63d33
Thomas Dunbar <rc0028614@student.riversidecollege.ac.uk>
2017-10-18 09:47:28 +0100

    Add project files.

---
+++ LISTS1/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LISTS1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+
+            InitializeComponent();
+            List<int> nums = new List<int>();
+            nums.Add(3);
+            nums.Add(5);
+            nums.Add(3);
+            foreach (int i in nums)
+            {
+                richTextBox1.AppendText(i + ",");
+            }
+            richTextBox1.AppendText("\n================\n");
+            nums.Remove(3);
+            foreach (int i in nums)
+            {
+                richTextBox1.AppendText(i + ",");
+            }
+             
+
+        }
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
